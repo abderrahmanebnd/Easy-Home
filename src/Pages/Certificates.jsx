a4c1091469f46ea6416d15c734132b5297415fc0
laponjade <dib.mehdi13@gmail.com>
2024-06-03 19:09:41 +0100

    changed login message

--- src/Pages/Certificates.jsx
+++ src/Pages/Certificates.jsx
@@ -3,13 +3,13 @@ import CertificateIdList from "../ui/CertificateIdList";
 
 function Certificates() {
   return (
-    <>
+    <div className="h-screen">
    <h2 className="mb-10 text-xl  font-semibold text-primaryColor sm:text-3xl">
         Verification : Certificates
   </h2>
   <CertificateIdList type="Certificate"/>
   
-  </>
+  </div>
     );
 }
 
